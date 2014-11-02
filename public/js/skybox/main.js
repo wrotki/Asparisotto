c18bc2d4b97b4272c318cfb3e9d7fec2408cc2af
mariusz <wrotki@msn.com>
2014-11-02 08:54:26 -0800

    Things completely wired up using Angular - clarifying comment

--- public/js/skybox/main.js
+++ public/js/skybox/main.js
@@ -1,11 +1,11 @@
 define(["skybox/SkyBox"], 
-        function(SkyBoxDef){
+        function(SkyBoxDef){ // SkyBoxDef is a function constructing the SkyBox class
             // Define SkyBox class here
             //
             var skyBoxModule = angular.module('SkyBox',[]);
             skyBoxModule.factory('skyBox',['mediaPath','threeDScene',function(mediaPath,threeDScene){
                 var injectables = { 'basePath': mediaPath };
-                var SkyBox = SkyBoxDef(injectables);
+                var SkyBox = SkyBoxDef(injectables); // SkyBoxDef is a function constructing the SkyBox class
                 var skyBox = new SkyBox({x: 0, y: 0, z: 0});
                 skyBox.basePath = mediaPath;
                 threeDScene.addActor(skyBox);
